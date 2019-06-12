8f2308e57a543e8358e92b92d2220266b59e271d
Denys Maciel <denysmaciel22@alu.ufc.br>
2019-06-12 19:35:59 -0300

    wordpress.sh

---
+++ wordpress.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Descobrindo Ip Public da Instancia:
+
+IP_Public="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
+
+# Intalando as dependências necessárias:
+
+sudo apt -y update
+
+sudo apt -y install php-curl php-gd php-mbstring php-xml php-xmlrpc php libapache2-mod-php php-mysql apache2 mysql-server
+
+#Criando o Banco de Dados do WordPress:
+
+sudo mysql<<EOF
+
+CREATE DATABASE wordpress;
+
+CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
+
+GRANT ALL ON wordpress.* TO 'wordpress'@'localhost';
+
+FLUSH PRIVILEGES;
+EOF
+
+# Baixando e Configurando o WordPress:
+
+wget https://wordpress.org/latest.tar.gz
+
+sudo mv latest.tar.gz /var/www/
+
+cd /var/www/
+
+sudo tar xpf latest.tar.gz
+
+cd
+
+sudo chown -R www-data:www-data /var/www/wordpress
+
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/wordpress.conf
+
+
+sudo sed -i '10s/^/        ServerName '$IP_Public'/' /etc/apache2/sites-available/wordpress.conf
+
+sudo sed -i '12s/html/wordpress/' /etc/apache2/sites-available/wordpress.conf
+
+sudo a2ensite wordpress.conf
+
+sudo systemctl reload apache2
