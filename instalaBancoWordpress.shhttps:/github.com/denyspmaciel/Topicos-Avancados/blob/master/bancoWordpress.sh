fe57e9e21457c1c0b39bef7bfd53133f0039d621
Denys Maciel <denysmaciel22@alu.ufc.br>
2019-06-14 11:49:59 -0300

    Update and rename instalaBancoWordpress.sh to instalaBancoWordpress.shhttps:/github.com/denyspmaciel/Topicos-Avancados/blob/master/bancoWordpress.sh

--- instalaBancoWordpress.shhttps:/github.com/denyspmaciel/Topicos-Avancados/blob/master/bancoWordpress.sh
+++ instalaBancoWordpress.shhttps:/github.com/denyspmaciel/Topicos-Avancados/blob/master/bancoWordpress.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-sudo apt-get update
-
-sudo apt -y install mysql-server
+apt-get update
+sudo apt -y install mysql-server \n",
 
 sudo mysql <<EOF
-
 CREATE DATABASE wordpress;
-CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';
-GRANT ALL ON wordpress.* TO 'wordpress'@'%';
+CREATE USER 'wp_admin'@'%' IDENTIFIED BY 'root';
+GRANT ALL ON wordpress.* TO 'wp_admin'@'%';
 FLUSH PRIVILEGES;
 EOF
 
 sudo sed -i 's/bind-address/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-
 sudo sed -i 's/skip-external-locking/#skip-external-locking/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-
 sudo systemctl restart mysql
