357bfac25260475b8d40babbe75fabbaaa5abfcb
Denys Maciel <denysmaciel22@alu.ufc.br>
2019-06-22 11:49:46 -0300

    Update wordAuto_bd_bastion.sh

--- wordAuto_bd_bastion.sh
+++ wordAuto_bd_bastion.sh
@@ -119,9 +119,9 @@ sudo sed -i "s/\$language = ''/\$language = 'pt_BR'/" /var/www/html/wordpress/wp
 
 ## ALTERANDO A VARIAVEL STEP PARA PASSAR IR PARA A TELA DE LOGIN
 
-caminho=`echo /var/www/html/wordpress/wp-admin/install.php`
-n=`grep -n '$step =' $caminho | cut -f 1 -d :`
-sed ""$n"s/: 0;/: 2;/" /var/www/html/wordpress/wp-admin/install.php
+#caminho=`echo /var/www/html/wordpress/wp-admin/install.php`
+n=`grep -n '$step =' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d :`
+sed -i ""$n"s/: 0;/: 2;/" /var/www/html/wordpress/wp-admin/install.php
 
 ## ALTERANDO AS CONFIGURACOES DO USUÁRIOS DE ACORDO COM OS PARAMETROS ESCOLHIDOS
 
