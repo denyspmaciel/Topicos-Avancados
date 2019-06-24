eb2312584d97b948bd825d1e02e731eae87d3135
Denys Maciel <denysmaciel22@alu.ufc.br>
2019-06-24 14:38:04 -0300

    Wordpress traduzido PT_BR

--- wordAuto_bd_bastion.sh
+++ wordAuto_bd_bastion.sh
@@ -60,34 +60,6 @@ sudo sed -i "s/password_here/root/g" /var/www/html/wordpress/wp-config.php
 sudo sed -i "s/localhost/$ip_banco/g" /var/www/html/wordpress/wp-config.php#!/bin/bash
 
 
-#INSTALA O O QUE É NECESSÁRIO
-sudo apt -y update
-sudo apt -y install php-curl php-gd php-mbstring php-xml php-xmlrpc apache2 php libapache2-mod-php php-mysql
-
-#ALGUMAS MODIFICAÇÕES NO APACHE2
-
-sudo sed -i "s/Options Indexes FollowSymLinks/Options FollowSymLinks/" /etc/apache2/apache2.conf
-sudo a2dissite 000-default.conf
-sudo systemctl stop apache2.service
-sudo systemctl start apache2.service
-sudo systemctl enable apache2.service
-
-#BAIXANDO WORDPRESS
-
-wget https://wordpress.org/latest.tar.gz
-tar -zxvf latest.tar.gz
-sudo mv wordpress /var/www/html/wordpress
-
-#SETANDO PERMIÇÕES NECESSÁRIAS
-sudo chown -R www-data:www-data /var/www/html/wordpress/
-sudo chmod -R 755 /var/www/html/wordpress/
-
-#ATIVA O SITE DO WORDPRESS.CONF
-
-sudo a2ensite wordpress.conf
-sudo a2enmod rewrite
-sudo systemctl restart apache2
-
 #CONFIGURANDO BD DO WORDPRESS
 
 sudo mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
